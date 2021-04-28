dcf9ea389bb5e40e2ec3413bfb955e1f6ac9c30d
gennadiy <magnit_gena@mail.ru>
2021-04-28 16:16:56 +0300

    start diploy

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,20 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../styles/theme';
 
 import '../styles/style.scss'
-import MainLayer from '../layer/default'
+import MainLayer from '../layer/Main'
+import TestLayer from '../layer/Test'
 import {AppBar} from "@material-ui/core";
+import { useRouter } from 'next/router'
 function MyApp({ Component, pageProps }) {
+    const router = useRouter()
+console.log('router', router)
 
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <MainLayer props={{...pageProps}}>
-                <Component {...pageProps} />
-            </MainLayer>
+          {router.route === '/test' ? 
+         <TestLayer props={{...pageProps}}><Component {...pageProps} /></TestLayer> : 
+          <MainLayer props={{...pageProps}}><Component {...pageProps} /></MainLayer>}
         </ThemeProvider>
     )
 }
---
+++ pages/contactss/index.js
@@ -0,0 +1,19 @@
+﻿const Contacts = (props) => {
+    return (
+        <h1 style={{textAlign:'center'}}>Страница Контакты </h1>
+    )
+    }
+    
+    export default Contacts
+    
+    
+    export async function getStaticProps(context) {
+        return {
+          props: {
+              title: 'Это тайтл страницы Контакты',
+              description: 'Это description страницы  Контакты',
+              keywords: 'Это keywords страницы  Контакты',
+          },
+          revalidate: 60
+        }
+      }
